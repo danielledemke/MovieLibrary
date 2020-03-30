9ee81ce077f83de3c36701f151d274c8660aa657
Danielle Demke <danielledemke@gmail.com>
2020-03-30 14:24:42 -0500

    Pull request

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -23,7 +23,8 @@ namespace WebAPISample.Controllers
         public IActionResult Get()
         {
             // Retrieve all movies from db logic
-            return Ok(new string[] { "movie1 string", "movie2 string" });
+            var movies = _context.Movies.Select(m => m.MovieId).ToList();
+            return Ok(movies);
         }
 
         // GET api/movie/5
